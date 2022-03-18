b84b042b409fd1b90265d74f23f215371efd9030
Anton13602 <an-kochetov@mail.ru>
2022-03-19 00:54:46 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  //throw new NotImplementedError('Not implemented');
+  let repeat = (options.repeatTimes !== undefined)? options.repeatTimes: 1
+  let separator = (options.separator !== undefined)? options.separator: '+'
+  let add = (options.addition !== undefined)? options.addition: ''
+  let additionNun = (options.additionRepeatTimes !== undefined)? options.additionRepeatTimes: 1
+  let additionSeparator = (options.additionSeparator !== undefined)? options.additionSeparator: '|'
+
+  let strAdd = (add + additionSeparator).repeat(`${additionNun}`)
+
+  let result = (str + strAdd.slice(0, strAdd.length - additionSeparator.length) + separator).repeat(`${repeat}`)
+
+  return result.slice(0, result.length - separator.length)
   // remove line with error and write your code here
 }
 
