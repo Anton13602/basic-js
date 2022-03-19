4eeb8d60a81e5ace35e7382ffc3bc0d1545b4b08
Anton13602 <an-kochetov@mail.ru>
2022-03-19 20:44:06 +0300

    feat: mine-sweeper, transform-array

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,8 +23,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  //throw new NotImplementedError('Not implemented');
+  if (matrix.length !== matrix[0].length) {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0 ; j < matrix[0].length; j++) {
+        if (typeof(matrix[i][j]) !== 'number') {
+          matrix[i][j] = 0
+        }
+      }
+    }
+    return matrix
+  }
+
+  
+  for (let i = 0; i < matrix.length - 1; i++) {
+    for (let j = 1 ; j < matrix[0].length - 1; j++) {
+      console.log(matrix[i][i] === matrix[i + 1][i + 1])
+      if (matrix[i][i] === matrix[i + 1][i + 1] && matrix[i][j] === matrix[i + 1][j - 1]) {
+        matrix[i][j] = 2 
+        matrix[i + 1][j - 1] = 2
+        break
+      }
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0 ; j < matrix[0].length; j++) {
+      if (typeof(matrix[i][j]) !== 'number') {
+        matrix[i][j] = 1
+      }
+    }
+  }
+  return matrix
   // remove line with error and write your code here
 }
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -14,12 +14,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  throw new NotImplementedError('Not implemented');
+  //throw new NotImplementedError('Not implemented');
   console.log(arr)
   if (!Array.isArray(arr)) {
     throw new Error(`'arr' parameter must be an instance of the Array!`)
     //return 'parameter must be an instance of the Array!';
   }
+  let arr2 = arr.slice()
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === '--discard-next') {
+      arr2[i] = undefined
+      arr2[i + 1] = undefined
+    }
+    if (arr2[i] === '--discard-prev') {
+      arr2[i] = undefined
+      arr2[i - 1] = undefined
+    }
+    if (arr2[i] === '--double-next') {
+      arr2[i] = arr[i + 1]
+    }
+    if (arr2[i] === '--double-prev') {
+      arr2[i] = arr2[i - 1]
+    }
+  }
+  let result = []
+  for (let val of arr2) {
+    if (val !== undefined) {
+      result.push(val)
+    }
+  }
+  console.log(result)
+  console.log(arr2)
+  return (result)
   // remove line with error and write your code here
 }
 
